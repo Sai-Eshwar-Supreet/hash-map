34102a175e8d1fce772cf24c0e39601021c8d10e
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-02-12 07:13:52 +0000

    Implement HashSet

---
+++ hash-set.js
@@ -0,0 +1,125 @@
+import { LinkedList } from "./linked-list/linked-list.js";
+
+
+class HashSet{
+    #loadFactor = 0.75;
+    #capacity = 16;
+    #buckets = [];
+    #length = 0;
+
+    constructor(){
+        this.#initialize();
+    }
+
+    #initialize(){
+        this.#buckets = [];
+        for(let i = 0; i < this.#capacity; i++){
+            this.#buckets[i] = new LinkedList();
+        }
+
+        console.log(`Initalized with capacity: ${this.#capacity}`);
+    }
+
+    hash(key){
+        let hashCode = 0;
+        const primeNumber = 31;
+
+        for(let i = 0; i< key.length; i++){
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+
+            hashCode %= this.#capacity;
+        }
+
+        return hashCode;
+    }
+
+    #isExpansionNeeded(){
+        const totalEntries = this.#buckets.reduce((prev, cur) => {
+            return prev + cur.size();
+        }, 0);
+
+        const loadFactor =  totalEntries / this.#capacity;
+
+        return loadFactor > this.#loadFactor;
+    }
+
+    #grow(){
+        const oldBuckets = this.#buckets;
+        this.#capacity *= 2;
+        this.#initialize();
+        
+        for(let oldBucket of oldBuckets){
+
+            while(oldBucket.size() !== 0){
+                const entry = oldBucket.pop();
+                this.set(entry);
+            }
+        }
+    }
+
+    #checkAndGrow(){
+        if(this.#isExpansionNeeded()){
+            this.#grow();
+        }
+    }
+
+    #getBucket(key){
+        const hash = this.hash(key);
+        return this.#buckets[hash];
+    }
+
+    set(key){
+        const bucket = this.#getBucket(key);
+        const index = bucket.findIndex(key);
+
+        if(bucket.contains(key)) return;
+        else{
+            bucket.append(key);
+            this.#length++;
+        }
+
+        this.#checkAndGrow();
+    }
+
+    has(key){
+        const bucket = this.#getBucket(key);
+
+        return bucket.contains(key);
+    }
+
+    remove(key){
+        const bucket = this.#getBucket(key);
+
+        const index = bucket.findIndex(key);
+
+        if(index === -1) return false;
+
+        bucket.removeAt(index);
+        this.#length--;
+
+        return true;
+    }
+
+    length(){
+        return this.#length;
+    }
+
+    clear(){
+        for(let bucket of this.#buckets){
+            bucket.clear();
+        }
+
+        this.#length = 0;
+    }
+
+    entries(){
+        const entries = [];
+        for(let bucket of this.#buckets){
+            entries.push(...bucket.toArray());
+        };
+
+        return entries;
+    }
+}
+
+export { HashSet }
--- sandbox.js
+++ sandbox.js
@@ -1,36 +1,90 @@
 import { HashMap  } from "./hash-map.js";
+import { HashSet } from "./hash-set.js";
 
-const map = new HashMap();
+// const map = new HashMap();
 
-console.log("The map object: ", map);
-console.log("The prototype object: ", Object.getPrototypeOf(map));
+// console.log("The map object: ", map);
+// console.log("The prototype object: ", Object.getPrototypeOf(map));
 
-// Test the hash map here
+// // Test the hash map here
 
-map.set("Carlos", "I am the old value");
-map.set("Carlos", "I am the new value");
-map.set("Sita", "I am Sita");
-map.set("Sita", "I am a woman");
-map.set("Rama", "I am Rama");
-map.set("Rama", "I am a man");
-map.set("Carlos", "I am another new value");
+// map.set("Carlos", "I am the old value");
+// map.set("Carlos", "I am the new value");
+// map.set("Sita", "I am Sita");
+// map.set("Sita", "I am a woman");
+// map.set("Rama", "I am Rama");
+// map.set("Rama", "I am a man");
+// map.set("Carlos", "I am another new value");
 
-console.log("Sita: ", map.get("Sita"));
-console.log("Gara: ", map.get("Gara"));
-console.log("Carlos: ", map.get("Carlos"));
+// console.log("Sita: ", map.get("Sita"));
+// console.log("Gara: ", map.get("Gara"));
+// console.log("Carlos: ", map.get("Carlos"));
 
-console.log("Has Sita: ", map.has("Sita"));
-console.log("Has Naruto: ", map.has("Naruto"));
+// console.log("Has Sita: ", map.has("Sita"));
+// console.log("Has Naruto: ", map.has("Naruto"));
 
-map.remove("Sita");
-console.log("Has Sita: ", map.has("Sita"));
+// map.remove("Sita");
+// console.log("Has Sita: ", map.has("Sita"));
 
-map.set("Sita", "New Sita entry");
-console.log("Has Sita: ", map.has("Sita"));
+// map.set("Sita", "New Sita entry");
+// console.log("Has Sita: ", map.has("Sita"));
 
-console.log("Length: ", map.length());
+// console.log("Length: ", map.length());
 
-console.log("Keys: ", map.keys());
-console.log("Values: ", map.values());
+// console.log("Keys: ", map.keys());
+// console.log("Values: ", map.values());
+// console.log("Entries: ", map.entries());
 
-map.clear();
+// map.clear();
+
+
+// Another test
+
+// const test = new HashMap()
+
+// test.set('apple', 'red')
+// test.set('banana', 'yellow')
+// test.set('carrot', 'orange')
+// test.set('dog', 'brown')
+// test.set('elephant', 'gray')
+// test.set('frog', 'green')
+// test.set('grape', 'purple')
+// test.set('hat', 'black')
+// test.set('ice cream', 'white')
+// test.set('jacket', 'blue')
+// test.set('kite', 'pink')
+// test.set('lion', 'golden')
+
+
+// test.set("ice cream", "choco");
+
+// test.set('moon', 'silver')
+
+
+// console.log("Entries: ", test.entries());
+
+
+const set = new HashSet();
+
+set.set("Carlos");
+set.set("Carlos");
+set.set("Sita");
+set.set("Sita");
+set.set("Rama");
+set.set("Rama");
+set.set("Carlos");
+
+console.log("Has Sita: ", set.has("Sita"));
+console.log("Has Naruto: ", set.has("Naruto"));
+
+set.remove("Sita");
+console.log("Has Sita: ", set.has("Sita"));
+
+set.set("Sita");
+console.log("Has Sita: ", set.has("Sita"));
+
+console.log("Length: ", set.length());
+console.log("Entries: ", set.entries());
+
+set.clear();
+console.log("Entries: ", set.entries());
