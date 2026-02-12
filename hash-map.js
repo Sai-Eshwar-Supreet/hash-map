128097995df11fc8e714a5c64ba655ca2159da36
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-02-12 03:35:54 +0000

    Initialize hash-map and sandbox

---
+++ hash-map.js
@@ -0,0 +1,6 @@
+class HashMap{
+    #loadFactor = 0.75;
+    #capacity = 16;
+}
+
+export { HashMap }
---
+++ sandbox.js
@@ -0,0 +1,8 @@
+import { HashMap  } from "./hash-map.js";
+
+const map = new HashMap();
+
+console.log("The map object: ", map);
+console.log("The prototype object: ", Object.getPrototypeOf(map));
+
+// Test the hash map here
